38ba5d7bf7ac07daf83c48f92b897e368d9accb1
Nguyen Ba Tu <tunb1993@gmail.com>
2015-11-14 22:23:01 +0700

    update

--- Source Code/Controllers/Shared/UploadController.cs
+++ Source Code/Controllers/Shared/UploadController.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 using System.IO;
-using System.Web;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using DomainModel;
-using System.Web.Script.Serialization;
 
 namespace Oneres.Controllers.Shared
 {
