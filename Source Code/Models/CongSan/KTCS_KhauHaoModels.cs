18077f76373597dda4c90a45233b2eced4a8f3c0
Nguyen Ba Tu <tunb1993@gmail.com>
2015-11-14 22:29:04 +0700

    u

--- Source Code/Models/CongSan/KTCS_KhauHaoModels.cs
+++ Source Code/Models/CongSan/KTCS_KhauHaoModels.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Collections.Specialized;
 using System.Data;
 using System.Data.SqlClient;
 using DomainModel;
