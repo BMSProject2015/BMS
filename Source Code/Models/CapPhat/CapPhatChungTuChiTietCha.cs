4209f7e66afeff302b0a2c39115f011d09d13a92
hungxuanls93 <hungpx2@gmail.com>
2015-11-21 14:42:11 +0700

    them file CT chi tiet cha

---
+++ Source Code/Models/CapPhat/CapPhatChungTuChiTietCha.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using DomainModel;
+using DomainModel.Abstract;
+
+namespace VIETTEL.Models
+{
+    public class CapPhatChungTuChiTietCha
+    {
+        /// <summary>
+        /// Lấy mã trạng thái (nếu) sau khi được duyệt của chứng từ
+        /// </summary>
+        /// <param name="sMaND">Mã người dùng</param>
+        /// <param name="siID_MaCapPhat">Mã chứng từ cấp phát</param>
+        /// <returns></returns>
+        public static int LayMaTrangThaiDuyetTrinhDuyet(String sMaND, String siID_MaCapPhat)
+        {
+            int vR = -1;
+            DataTable dt = CapPhat_ChungTuModels.LayChungTuCapPhat(siID_MaCapPhat);
+            int iID_MaTrangThaiDuyet = Convert.ToInt32(dt.Rows[0]["iID_MaTrangThaiDuyet"]);
+            dt.Dispose();
+            if (LuongCongViecModel.NguoiDung_DuocSuaChungTu(PhanHeModels.iID_MaPhanHeCapPhat, sMaND, iID_MaTrangThaiDuyet))
+            {
+                int iID_MaTrangThaiDuyet_TrinhDuyet = LuongCongViecModel.Luong_iID_MaTrangThaiDuyet_TrinhDuyet(iID_MaTrangThaiDuyet);
+                if (iID_MaTrangThaiDuyet_TrinhDuyet > 0)
+                {
+                    vR = Convert.ToInt32(iID_MaTrangThaiDuyet_TrinhDuyet);
+                }
+            }
+            return vR;
+        }
+
+        /// <summary>
+        ///  Lấy mã trạng thái (nếu) sau khi bị từ chối của chứng từ
+        /// </summary>
+        /// <param name="sMaND"></param>
+        /// <param name="siID_MaCapPhat"></param>
+        /// <returns></returns>
+        public static int LayMaTrangThaiDuyetTuChoi(String sMaND, String siID_MaCapPhat)
+        {
+            int vR = -1;
+            DataTable dt = CapPhat_ChungTuModels.LayChungTuCapPhat(siID_MaCapPhat);
+            int iID_MaTrangThaiDuyet = Convert.ToInt32(dt.Rows[0]["iID_MaTrangThaiDuyet"]);
+            dt.Dispose();
+            if (LuongCongViecModel.NguoiDung_DuocSuaChungTu(PhanHeModels.iID_MaPhanHeCapPhat, sMaND, iID_MaTrangThaiDuyet))
+            {
+                int iID_MaTrangThaiDuyet_TuChoi = LuongCongViecModel.Luong_iID_MaTrangThaiDuyet_TuChoi(iID_MaTrangThaiDuyet);
+                if (iID_MaTrangThaiDuyet_TuChoi > 0)
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT Count(*) FROM CP_CapPhatChiTiet WHERE iID_MaCapPhat=@iID_MaCapPhat AND bDongY=0");
+                    cmd.Parameters.AddWithValue("@iID_MaCapPhat", siID_MaCapPhat);
+                    if (Convert.ToInt32(Connection.GetValue(cmd, 0)) > 0)
+                    {
+                        vR = Convert.ToInt32(iID_MaTrangThaiDuyet_TuChoi);
+                    }
+                    cmd.Dispose();
+                }
+            }
+            return vR;
+        }
+    }
+}
